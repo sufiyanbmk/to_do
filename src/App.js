47a5183577432c0f06c93fe1d2b17b4a0a40e9d0
adarsht00001 <adarsht00001@gmail.com>
2022-12-14 19:28:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+  const [toDos,setTodos]=useState([])
+  const [task,setTask]=useState('')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input value={task} onChange={(e)=>setTask(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={()=>setTodos([...toDos,{id:Date.now(),task,status:false}])} className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+        {
+          toDos.map((element)=>{
+            return( 
+            <div className="todo" key={element.id}>
+            <div className="left">
+              <input
+              onChange={(e)=>{
+                setTodos(toDos.filter((obj)=>{
+                  if(obj.id === element.id){
+                    obj.status=e.target.checked
+                  }
+                  return obj
+                }))
+              }} 
+              value={element.status} type="checkbox" name="" id="" />
+              <p>{element.task}</p>
+            </div>
+            <div className="right">
+              <i className="fas fa-times"></i>
+            </div>
+          </div>)
+          })
+        }
+       
+      </div>
     </div>
   );
 }
