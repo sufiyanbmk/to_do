2e21d8992e95486b3b86a5c7a250208f2e467d4d
adarsht00001 <adarsht00001@gmail.com>
2022-12-15 09:19:39 +0530

    'completed task'

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 function App() {
-  const [toDos,setTodos]=useState([])
-  const [task,setTask]=useState('')
+  const [toDos, setTodos] = useState([]);
+  const [task, setTask] = useState("");
   return (
     <div className="app">
       <div className="mainHeading">
@@ -13,37 +13,68 @@ function App() {
         <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
       </div>
       <div className="input">
-        <input value={task} onChange={(e)=>setTask(e.target.value)} type="text" placeholder="🖊️ Add item..." />
-        <i onClick={()=>setTodos([...toDos,{id:Date.now(),task,status:false}])} className="fas fa-plus"></i>
+        <input
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+          type="text"
+          placeholder="🖊️ Add item..."
+        />
+        <i
+          onClick={() =>
+            setTodos([...toDos, { id: Date.now(), task, status: false }])
+          }
+          className="fas fa-plus"
+        ></i>
       </div>
       <div className="todos">
-        {
-          toDos.map((element)=>{
-            return( 
+        {toDos.map((element) => {
+          return (
             <div className="todo" key={element.id}>
-            <div className="left">
-              <input
-              onChange={(e)=>{
-                setTodos(toDos.filter((obj)=>{
-                  if(obj.id === element.id){
-                    obj.status=e.target.checked
-                  }
-                  return obj
-                }))
-              }} 
-              value={element.status} type="checkbox" name="" id="" />
-              <p>{element.task}</p>
+              <div className="left">
+                <input
+                  onChange={(e) => {
+                    setTodos(
+                      toDos.filter((obj) => {
+                        if (obj.id === element.id) {
+                          obj.status = e.target.checked;
+                        }
+                        return obj;
+                      })
+                    );
+                  }}
+                  value={element.status}
+                  type="checkbox"
+                  name=""
+                  id=""
+                />
+                <p>{element.task}</p>
+              </div>
+              <div className="right">
+                <i className="fas fa-times"></i>
+              </div>
             </div>
-            <div className="right">
-              <i className="fas fa-times"></i>
-            </div>
-          </div>)
-          })
-        }
-       
+          );
+        })}
       </div>
+      <Completed />
     </div>
   );
+
+  function Completed() {
+    return(
+    <div style={{ paddingTop:50}}>
+      <h2>Completed Tasks</h2>
+      {toDos.map((element) => {
+        if (element.status) {
+          return (<h2>{element.task}</h2>)
+        }
+        else{
+          return null
+        }
+      })}
+    </div>
+    )
+  }
 }
 
 export default App;
