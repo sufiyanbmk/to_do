924727ee8d95e2156cd46b60d9568b4f24895c6c
sufiyanbmk <sufiyanbmk@gmail.com>
2023-01-13 20:07:12 +0530

    update and validation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import "./App.css";
-import { useState } from "react";
-import Swal from 'sweetalert2'
-import withReactContent from 'sweetalert2-react-content';
-const MySwal = withReactContent(Swal)
+import React, { Fragment, useState } from "react";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+const MySwal = withReactContent(Swal);
 
 function App() {
   const [toDos, setTodos] = useState([]);
   const [task, setTask] = useState("");
   const [deleted, setDeleted] = useState([]);
 
-  function validate(value){
-    if(value.length>0){
-      setTodos([...toDos, { id: Date.now(), task:value, status: false }])
-      setTask('')
-    }
-    else{
+  function validate(value) {
+    if (value.length > 0) {
+      setTodos([
+        ...toDos,
+        { id: Date.now(), task: value, status: false, isEditing: false },
+      ]);
+      setTask("");
+    } else {
       MySwal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: 'Please enter a value!',
-      })
+        icon: "error",
+        title: "Oops...",
+        text: "Please enter a value!",
+      });
     }
+  }
+
+  function handleEdit(id) {
+    setTodos(
+      toDos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, isEditing: true };
+        }
+        return todo;
+      })
+    );
+  }
+
+  function handleSubmit(event, id) {
+    event.preventDefault();
+    const newText = event.target.elements.text.value;
+    console.log(newText)
+    setTodos(
+      toDos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, task: newText, isEditing: false };
+        }
+        return todo;
+      })
+    );
   }
 
   return (
@@ -39,17 +66,18 @@ function App() {
           type="text"
           placeholder="🖊️ Add item..."
         />
-        <i
-          onClick={() =>
-            validate(task)
-          }
-          className="fas fa-plus"
-        ></i>
+        <i onClick={() => validate(task)} className="fas fa-plus"></i>
       </div>
       <div className="todos">
         {toDos.map((element) => {
           return (
             <div className="todo" key={element.id}>
+              {element.isEditing ? (
+                <form onSubmit={(event) => handleSubmit(event, element.id)}>
+                  <input name="text" defaultValue={element.task} />
+                  <button type="submit">Save</button>
+                </form>
+              ) : (
               <div className="left">
                 <input
                   onChange={(e) => {
@@ -69,6 +97,8 @@ function App() {
                 />
                 <p>{element.task}</p>
               </div>
+              )}
+              <button onClick={() => handleEdit(element.id)}>Edit</button>
               <div className="right">
                 <i
                   onClick={() =>
@@ -114,7 +144,7 @@ function App() {
       <div style={{ paddingTop: 50 }}>
         <h2>Deleted Tasks</h2>
         {deleted.map((element) => {
-            return <h2 key={element.id}>{element.task}</h2>;
+          return <h2 key={element.id}>{element.task}</h2>;
         })}
       </div>
     );
